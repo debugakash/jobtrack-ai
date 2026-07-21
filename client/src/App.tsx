244f6cb17db8ff1b2873d6cc74e137e5d3814e0e
Akash Arya <akasharya353@gmail.com>
2026-07-21 17:00:30 +0530

    chore: initialize React frontend with Tailwing and shadcn

---
+++ client/src/App.tsx
@@ -0,0 +1,18 @@
+// import "./App.css";
+
+// function App() {
+//   return (
+//     <div className="flex min-h-screen items-center justify-center bg-slate-100">
+//       <h1 className="text-5xl font-bold text-blue-600">JobTrack AI 🚀</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
+import { test } from "@/lib/test";
+
+function App() {
+  return <h1>{test}</h1>;
+}
+
+export default App;
